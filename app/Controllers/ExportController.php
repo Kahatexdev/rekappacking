f51bd9dfa9546a8e8343058a9b70044b998e3018
novandra <75214830+notfoundra@users.noreply.github.com>
2024-01-12 09:03:20 +0700

    ko ga ke savve

--- app/Controllers/ExportController.php
+++ app/Controllers/ExportController.php
@@ -2,100 +2,10 @@
 
 namespace App\Controllers;
 
-use App\Models\ProductionModel;
 use TCPDF;
 
 class ExportController extends BaseController
 {
-    // public function exportMesin($selectedIds)
-    // {
-    //     $data = $this->getData($selectedIds);
-    //     // Create a new PDF document
-    //     $pdf = new TCPDF();
-
-    //     // Set document information
-    //     $pdf->SetCreator('RPL Team');
-    //     $pdf->SetAuthor('Saha');
-    //     $pdf->SetTitle('Export Data Mesin');
-    //     $pdf->SetSubject('Data Mesin PDF Export');
-
-    //     // Set margins
-    //     $pdf->SetMargins(10, 10, 10);
-
-    //     // Add a page
-    //     $pdf->AddPage();
-
-    //     // Set background image
-    //     // $backgroundImagePath = FCPATH . 'public/assets/images/bg2.png';
-    //     //   $pdf->Image($backgroundImagePath, 0, 0, $pdf->getPageWidth(), $pdf->getPageHeight(), '', '', '', false, 300, '', false, false, 0);
-
-    //     // Set header
-    //     $pdf->setHeaderData('', 0, 'Data Export', 'export Data');
-
-    //     // Set footer
-    //     $pdf->setFooterData(['0' => ['text' => 'Page {PAGENO}', 'align' => 'right']], ['0' => ['text' => 'Footer Text']]);
-
-    //     // Set content
-    //     $html = '<h1>Data Mesin</h1>';
-    //     $html .= '<table border="1">';
-    //     $html .= '<tr><th>No Model</th><th>Inisial</th><th>Style</th><th>Colour</th><th>ID Production</th><th>Tanggal Production</th><th>Bagian</th><th>Qty Production</th><th>BS Production</th><th>No Box</th><th>No Label</th><th>Tanggal Upload</th></tr>';
-
-    //     foreach ($data['dataProd'] as $row) {
-    //         $html .= '<tr>';
-    //         $html .= '<td>' . $row['no_model'] . '</td>';
-    //         $html .= '<td>' . $row['inisial'] . '</td>';
-    //         $html .= '<td>' . $row['style'] . '</td>';
-    //         $html .= '<td>' . $row['colour'] . '</td>';
-    //         $html .= '<td>' . $row['id_production'] . '</td>';
-    //         $html .= '<td>' . $row['tgl_prod'] . '</td>';
-    //         $html .= '<td>' . $row['bagian'] . '</td>';
-    //         $html .= '<td>' . $row['qty_prod'] . '</td>';
-    //         $html .= '<td>' . $row['bs_prod'] . '</td>';
-    //         $html .= '<td>' . $row['no_box'] . '</td>';
-    //         $html .= '<td>' . $row['no_label'] . '</td>';
-    //         $html .= '<td>' . $row['tgl_upload'] . '</td>';
-    //         $html .= '</tr>';
-    //     }
-
-    //     $html .= '</table>';
-    //     $pdf->writeHTML($html, true, false, true, false, '');
-
-    //     // Output PDF to the browser or save it to a file
-    //     $pdf->Output('export_data_mesin.pdf', 'I'); // 'I' to send the file inline to the browser
-    // }
-
-
-    // private function getData($selectedIds)
-    // {
-    //     $selectedIdsArray = explode(',', $selectedIds);
-    //     $dataProd = $this->prodModel->find($selectedIdsArray);
-    //     $dataJoined = [];
-
-    //     foreach ($dataProd as $row) {
-    //         $kode_shipment = $row['kode_shipment'];
-    //         $idInisial = $this->shipment->getIdInisial($kode_shipment);
-    //         $dataInisial = $this->masterInisial->where('id_inisial', intval($idInisial['id_inisial']))->first();
-    //         $dataJoined[] = [
-    //             'no_model'  => $dataInisial['no_model'],
-    //             'inisial'   => $dataInisial['inisial'],
-    //             'style'     => $dataInisial['style'],
-    //             'colour'    => $dataInisial['colour'],
-    //             'id_production' => $row['id_production'],
-    //             'tgl_prod'  => $row['tgl_prod'],
-    //             'bagian'    => $row['bagian'],
-    //             'qty_prod'  => $row['qty_prod'],
-    //             'bs_prod'   => $row['bs_prod'],
-    //             'no_box'    => $row['no_box'],
-    //             'no_label'  => $row['no_label'],
-    //             'tgl_upload' => $row['created_at'],
-    //         ];
-    //     }
-
-    //     return [
-    //         'dataProd' => $dataJoined,
-    //         'judul' => 'export data mesin'
-    //     ];
-    // }
     function exportMesin($selectedIds)
     {
         $pdf = new TCPDF();
@@ -144,6 +54,9 @@ class ExportController extends BaseController
         $pdf->Cell(0, 10, 'Export Data Produksi Mesin', 0, 1, 'C');
         // Output the HTML content to the PDF
 
+        $pdf->SetFont('times', 'B', 10);
+        $admin = session()->get('username');
+        $pdf->Cell(0, 10, 'Admin : ' . $admin, 0, 1, 'L');
         // HTML table
         $pdf->SetFont(
             'times',
@@ -200,5 +113,6 @@ class ExportController extends BaseController
         $filePath = $directory . '' . $nama . '.pdf';
         // Save the PDF to the specified directory
         $pdf->Output($filePath, 'f');
+        return redirect()->to('packing/datamesin');
     }
 }
